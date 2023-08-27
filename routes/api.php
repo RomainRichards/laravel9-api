33f1427fd4444fe19e1500e51412e72aa642cb89
Romain Richards <96961208+RomainRichards@users.noreply.github.com>
2023-08-26 20:40:48 -0400

    Create students table

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('students', function() {
-    return 'this is student api';
-});
+
