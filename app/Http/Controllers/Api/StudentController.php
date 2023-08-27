83194a3552b911ed6e49e039a74dbbf64705ec7b
Romain Richards <96961208+RomainRichards@users.noreply.github.com>
2023-08-26 22:42:37 -0400

    Created functions to manipulate api data
    Store, Show, Edit, Destroy(delete), Update

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -25,6 +25,7 @@ class StudentController extends Controller
         } 
     }
 
+    // Function to store api data with different validators
     public function store(Request $request) 
     {
         $validator = Validator::make($request->all(), [
@@ -59,6 +60,109 @@ class StudentController extends Controller
                     'message' => "Something Went Wrong!"
                 ], 500);
             }
+        }
+    }
+
+    // Function to display requested data using record id.
+    public function show($id)
+    {
+        $student = Student::find($id);
+        if($student){
+
+            return response()->json([
+                'status' => 200,
+                'student' => $student
+            ], 200);  
+
+        }else{
+            
+            return response()->json([
+                'status' => 404,
+                'message' => "No Such Student Found!"
+            ], 404);
+            
+        }
+    }
+
+    public function edit($id)
+    {
+        $student = Student::find($id);
+        if($student){
+
+            return response()->json([
+                'status' => 200,
+                'student' => $student
+            ], 200);  
+
+        }else{
+            
+            return response()->json([
+                'status' => 404,
+                'message' => "No Such Student Found!"
+            ], 404);
+            
+        }
+    }
+
+    // Find the student then update the record.
+    public function update(Request $request, int $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:191',
+            'course' => 'required|string|max:191',
+            'email' => 'required|email|max:191',
+            'phone' => 'required|digits:10',
+        ]);
+
+        if($validator-> fails()) {
+            return response() -> json([
+                'status' => 422,
+                'errors' => $validator -> messages() 
+            ], 422);
+        } else{
+
+            $student = Student::find($id); 
+            if($student){
+
+                $student-> update([
+                    'name'=> $request -> name,
+                    'course'=> $request -> course,
+                    'email'=> $request -> email,
+                    'phone'=> $request -> phone,
+                ]);
+
+                return response()->json([
+                    'status' => 200,
+                    'message' => "Student updated Successfully"
+                ], 200);
+            }else{
+                return response()->json([
+                    'status' => 404,
+                    'message' => "No Such Student Found!"
+                ], 404);
+            }
+        }
+    }
+
+    // Find the student then delete the record.
+    public function destroy($id)
+    {
+        $student = Student::find($id);
+        if($student){
+
+            $student->delete();
+            return response()->json([
+                'status' => 200,
+                'message' => "Student Deleted Successfully!"
+            ], 200);
+
+        }else{
+
+            return response()->json([
+                'status' => 404,
+                'message' => "No Such Student Found!"
+            ], 404);
+
         }
     }
 }
