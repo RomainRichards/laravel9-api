eab6c4ef09da1a8b525eae73495735754e2b0f7b
Romain Richards <96961208+RomainRichards@users.noreply.github.com>
2023-08-26 21:53:41 -0400

    Create function to store api data and different validations

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Models\Student;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class StudentController extends Controller
 {
@@ -21,8 +22,43 @@ class StudentController extends Controller
                 'status' => 404,
                 'message' => 'No Records Found'
             ], 404);
-        }
+        } 
+    }
 
-        
+    public function store(Request $request) 
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:191',
+            'course' => 'required|string|max:191',
+            'email' => 'required|email|max:191',
+            'phone' => 'required|digits:10',
+        ]);
+
+        if($validator-> fails()) {
+            return response() -> json([
+                'status' => 422,
+                'errors' => $validator -> messages() 
+            ], 422);
+        } else{
+
+            $student = Student::create([
+                'name'=> $request -> name,
+                'course'=> $request -> course,
+                'email'=> $request -> email,
+                'phone'=> $request -> phone,
+            ]);
+
+            if($student){
+                return response()->json([
+                    'status' => 200,
+                    'message' => "Student Created Successfully"
+                ], 200);
+            }else{
+                return response()->json([
+                    'status' => 500,
+                    'message' => "Something Went Wrong!"
+                ], 500);
+            }
+        }
     }
 }
